234ad7b24cf257f7eb9cfa7fd8f97a29018a8e34
swami <swami@lenovo.mshome.net>
2024-01-08 16:15:42 +0530

    First upload to github

---
+++ UploadingProjectOnGitHub/src/main/java/com/testing/UploadingProjectOnGitHubApplication.java
@@ -0,0 +1,15 @@
+package com.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadingProjectOnGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadingProjectOnGitHubApplication.class, args);
+		
+		System.out.println("This is a testing app");
+	}
+
+}
